9dbd70a5f73ca82df2ccaeab873c7bdc22951270
tgallant <tim@lilt.com>
2025-12-02 16:51:26 -0800

    day 02 pt 2

--- src/02/solution.lisp
+++ src/02/solution.lisp
@@ -30,6 +30,37 @@
                    do (setq sum (+ sum i)))
         finally (return sum)))
 
+(defun split-into-chunks (seq size)
+  (loop for i from 0 by size to (length seq)
+        as chunk = (subseq seq i (min (+ i size) (length seq)))
+        while chunk
+        collect chunk))
+
+(defun all-sublists-equal-p (list-of-lists)
+  (if (null list-of-lists) t
+      (let ((first-sublist (car list-of-lists)))
+        (every #'(lambda (sublist) (equal first-sublist sublist))
+               (cdr list-of-lists)))))
+
+(defun dubp-v2 (i)
+  (loop with seq = (str:split "" (write-to-string i) :omit-nulls t)
+        with len = (length seq)
+        for n from 1 to (/ len 2)
+        if (-> (split-into-chunks seq n)
+               (all-sublists-equal-p))
+          do (return t)))
+
+(defun pt2 (input)
+  (loop with ranges = (->> (uiop:read-file-string input)
+                           (str:split ",")
+                           (mapcar #'parse-range))
+        with sum = 0
+        for (lo . hi) in ranges
+        do (loop for i from lo to hi
+                 if (dubp-v2 i)
+                   do (setq sum (+ sum i)))
+        finally (return sum)))
+
 (def-suite 02-solution)
 (in-suite 02-solution)
 
@@ -38,6 +69,12 @@
 
 (test pt1-input
   (is (= (pt1 "input.txt") 40055209690)))
+
+(test pt2-test
+  (is (= (pt2 "test.txt") 4174379265)))
+
+(test pt2-input
+  (is (= (pt2 "input.txt") 50857215650)))
 
 (defun main ()
   (run! '02-solution))
