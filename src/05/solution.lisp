b910b7a3f3ac610bfa99b61ebcff29362a649d59
tgallant <me@timgallant.us>
2025-12-05 17:38:19 -0800

    day 05 pt2

--- src/05/solution.lisp
+++ src/05/solution.lisp
@@ -40,6 +40,42 @@
           do (incf total)
         finally (return total)))
 
+(defun merge-ranges (r1 r2)
+  (bind (((lo1 hi1) r1)
+         ((lo2 hi2) r2))
+    (when (and (>= lo2 lo1)
+               (<= lo2 hi1))
+      (list lo1 (max hi1 hi2)))))
+
+(defun sum-ranges (ranges)
+  (loop with total = 0
+        for (lo hi) in ranges
+        do (setq total (+ total (1+ (- hi lo))))
+        finally (return total)))
+
+(defun pt2 (input)
+  (loop with data = (-> (uiop:read-file-lines input)
+                        (parse-inv))
+        with ranges = (-> (nth 0 data)
+                          (sort (lambda (a b)
+                                  (< (car a) (car b)))))
+        with cur = nil
+        with merged = nil
+        for range in ranges
+        if (eq cur nil)
+          do (setq cur range)
+        else
+          do (-> (merge-ranges cur range)
+                 (lambda (res)
+                   (if (eq res nil)
+                       (progn
+                         (push cur merged)
+                         (setq cur range))
+                       (setq cur res))))
+        finally (-> (push cur merged)
+                    (sum-ranges)
+                    (return))))
+
 (def-suite 05-solution)
 (in-suite 05-solution)
 
@@ -48,6 +84,12 @@
 
 (test pt1-input
   (is (= (pt1 "input.txt") 611)))
+
+(test pt2-test
+  (is (= (pt2 "test.txt") 14)))
+
+(test pt2-input
+  (is (= (pt2 "input.txt") 345995423801866)))
 
 (defun main ()
   (run! '05-solution))
